472c89923727f7d07a008f88759b7ea90e3737a3
yipan <ypan1988@gmail.com>
2016-06-02 14:22:53 +0100

    add .lang-format

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -9,15 +9,18 @@
 #'@param Z model matrix for the diagonal matrix.
 #'@param W model matrix for the lower triangular matrix.
 #'@param start starting values for the parameters in the model.
-#'@param trace the values of the objective function and the parameters are printed for all the trace'th iterations.
-#'@param profile whether parameters should be estimated sequentially using the idea of profile likelihood or not.
+#'@param trace the values of the objective function and the parameters are
+NULL
+
+#'@param profile whether parameters should be estimated sequentially using the
+NULL
+
 #'@param errorMsg whether or not the error message should be print.
-#'@seealso \code{\link{acd_estimation}} for joint mean covariance model fitting based on ACD,
-#' \code{\link{hpc_estimation}} for joint mean covariance model fitting based on HPC.
-#'@export
-mcd_estimation <- function(m, Y, X, Z, W, start, trace = FALSE, profile = TRUE, errorMsg = FALSE) {
-    .Call('jmcm_mcd_estimation', PACKAGE = 'jmcm', m, Y, X, Z, W, start, trace, profile, errorMsg)
-}
+#'@seealso \code{\link{acd_estimation}} for joint mean covariance model fitting
+NULL
+
+#' \code{\link{hpc_estimation}} for joint mean covariance model fitting based
+NULL
 
 #'@title Fit Joint Mean-Covariance Models based on ACD
 #'@description Fit joint mean-covariance models based on ACD.
@@ -27,15 +30,18 @@ mcd_estimation <- function(m, Y, X, Z, W, start, trace = FALSE, profile = TRUE, 
 #'@param Z model matrix for the diagonal matrix.
 #'@param W model matrix for the lower triangular matrix.
 #'@param start starting values for the parameters in the model.
-#'@param trace the values of the objective function and the parameters are printed for all the trace'th iterations.
-#'@param profile whether parameters should be estimated sequentially using the idea of profile likelihood or not.
+#'@param trace the values of the objective function and the parameters are
+NULL
+
+#'@param profile whether parameters should be estimated sequentially using the
+NULL
+
 #'@param errorMsg whether or not the error message should be print.
-#'@seealso \code{\link{mcd_estimation}} for joint mean covariance model fitting based on MCD,
-#' \code{\link{hpc_estimation}} for joint mean covariance model fitting based on HPC.
-#'@export
-acd_estimation <- function(m, Y, X, Z, W, start, trace = FALSE, profile = TRUE, errorMsg = FALSE) {
-    .Call('jmcm_acd_estimation', PACKAGE = 'jmcm', m, Y, X, Z, W, start, trace, profile, errorMsg)
-}
+#'@seealso \code{\link{mcd_estimation}} for joint mean covariance model fitting
+NULL
+
+#' \code{\link{hpc_estimation}} for joint mean covariance model fitting based
+NULL
 
 #'@title Fit Joint Mean-Covariance Models based on HPC
 #'@description Fit joint mean-covariance models based on HPC.
@@ -45,11 +51,29 @@ acd_estimation <- function(m, Y, X, Z, W, start, trace = FALSE, profile = TRUE, 
 #'@param Z model matrix for the diagonal matrix.
 #'@param W model matrix for the lower triangular matrix.
 #'@param start starting values for the parameters in the model.
-#'@param trace the values of the objective function and the parameters are printed for all the trace'th iterations.
-#'@param profile whether parameters should be estimated sequentially using the idea of profile likelihood or not.
+#'@param trace the values of the objective function and the parameters are
+NULL
+
+#'@param profile whether parameters should be estimated sequentially using the
+NULL
+
 #'@param errorMsg whether or not the error message should be print.
-#'@seealso \code{\link{mcd_estimation}} for joint mean covariance model fitting based on MCD,
-#' \code{\link{acd_estimation}} for joint mean covariance model fitting based on ACD.
+#'@seealso \code{\link{mcd_estimation}} for joint mean covariance model fitting
+NULL
+
+#' \code{\link{acd_estimation}} for joint mean covariance model fitting based
+NULL
+
+#'@export
+mcd_estimation <- function(m, Y, X, Z, W, start, trace = FALSE, profile = TRUE, errorMsg = FALSE) {
+    .Call('jmcm_mcd_estimation', PACKAGE = 'jmcm', m, Y, X, Z, W, start, trace, profile, errorMsg)
+}
+
+#'@export
+acd_estimation <- function(m, Y, X, Z, W, start, trace = FALSE, profile = TRUE, errorMsg = FALSE) {
+    .Call('jmcm_acd_estimation', PACKAGE = 'jmcm', m, Y, X, Z, W, start, trace, profile, errorMsg)
+}
+
 #'@export
 hpc_estimation <- function(m, Y, X, Z, W, start, trace = FALSE, profile = TRUE, errorMsg = FALSE) {
     .Call('jmcm_hpc_estimation', PACKAGE = 'jmcm', m, Y, X, Z, W, start, trace, profile, errorMsg)
--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -7,77 +7,59 @@
 using namespace Rcpp;
 
 // mcd_estimation
-Rcpp::List mcd_estimation(arma::vec m, arma::vec Y, arma::mat X, arma::mat Z,
-                          arma::mat W, arma::vec start, bool trace,
-                          bool profile, bool errorMsg);
-RcppExport SEXP jmcm_mcd_estimation(SEXP mSEXP, SEXP YSEXP, SEXP XSEXP,
-                                    SEXP ZSEXP, SEXP WSEXP, SEXP startSEXP,
-                                    SEXP traceSEXP, SEXP profileSEXP,
-                                    SEXP errorMsgSEXP) {
-  BEGIN_RCPP
-  Rcpp::RObject __result;
-  Rcpp::RNGScope __rngScope;
-  Rcpp::traits::input_parameter<arma::vec>::type m(mSEXP);
-  Rcpp::traits::input_parameter<arma::vec>::type Y(YSEXP);
-  Rcpp::traits::input_parameter<arma::mat>::type X(XSEXP);
-  Rcpp::traits::input_parameter<arma::mat>::type Z(ZSEXP);
-  Rcpp::traits::input_parameter<arma::mat>::type W(WSEXP);
-  Rcpp::traits::input_parameter<arma::vec>::type start(startSEXP);
-  Rcpp::traits::input_parameter<bool>::type trace(traceSEXP);
-  Rcpp::traits::input_parameter<bool>::type profile(profileSEXP);
-  Rcpp::traits::input_parameter<bool>::type errorMsg(errorMsgSEXP);
-  __result = Rcpp::wrap(
-      mcd_estimation(m, Y, X, Z, W, start, trace, profile, errorMsg));
-  return __result;
-  END_RCPP
+Rcpp::List mcd_estimation(arma::vec m, arma::vec Y, arma::mat X, arma::mat Z, arma::mat W, arma::vec start, bool trace, bool profile, bool errorMsg);
+RcppExport SEXP jmcm_mcd_estimation(SEXP mSEXP, SEXP YSEXP, SEXP XSEXP, SEXP ZSEXP, SEXP WSEXP, SEXP startSEXP, SEXP traceSEXP, SEXP profileSEXP, SEXP errorMsgSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< arma::vec >::type m(mSEXP);
+    Rcpp::traits::input_parameter< arma::vec >::type Y(YSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type X(XSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type Z(ZSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type W(WSEXP);
+    Rcpp::traits::input_parameter< arma::vec >::type start(startSEXP);
+    Rcpp::traits::input_parameter< bool >::type trace(traceSEXP);
+    Rcpp::traits::input_parameter< bool >::type profile(profileSEXP);
+    Rcpp::traits::input_parameter< bool >::type errorMsg(errorMsgSEXP);
+    __result = Rcpp::wrap(mcd_estimation(m, Y, X, Z, W, start, trace, profile, errorMsg));
+    return __result;
+END_RCPP
 }
 // acd_estimation
-Rcpp::List acd_estimation(arma::vec m, arma::vec Y, arma::mat X, arma::mat Z,
-                          arma::mat W, arma::vec start, bool trace,
-                          bool profile, bool errorMsg);
-RcppExport SEXP jmcm_acd_estimation(SEXP mSEXP, SEXP YSEXP, SEXP XSEXP,
-                                    SEXP ZSEXP, SEXP WSEXP, SEXP startSEXP,
-                                    SEXP traceSEXP, SEXP profileSEXP,
-                                    SEXP errorMsgSEXP) {
-  BEGIN_RCPP
-  Rcpp::RObject __result;
-  Rcpp::RNGScope __rngScope;
-  Rcpp::traits::input_parameter<arma::vec>::type m(mSEXP);
-  Rcpp::traits::input_parameter<arma::vec>::type Y(YSEXP);
-  Rcpp::traits::input_parameter<arma::mat>::type X(XSEXP);
-  Rcpp::traits::input_parameter<arma::mat>::type Z(ZSEXP);
-  Rcpp::traits::input_parameter<arma::mat>::type W(WSEXP);
-  Rcpp::traits::input_parameter<arma::vec>::type start(startSEXP);
-  Rcpp::traits::input_parameter<bool>::type trace(traceSEXP);
-  Rcpp::traits::input_parameter<bool>::type profile(profileSEXP);
-  Rcpp::traits::input_parameter<bool>::type errorMsg(errorMsgSEXP);
-  __result = Rcpp::wrap(
-      acd_estimation(m, Y, X, Z, W, start, trace, profile, errorMsg));
-  return __result;
-  END_RCPP
+Rcpp::List acd_estimation(arma::vec m, arma::vec Y, arma::mat X, arma::mat Z, arma::mat W, arma::vec start, bool trace, bool profile, bool errorMsg);
+RcppExport SEXP jmcm_acd_estimation(SEXP mSEXP, SEXP YSEXP, SEXP XSEXP, SEXP ZSEXP, SEXP WSEXP, SEXP startSEXP, SEXP traceSEXP, SEXP profileSEXP, SEXP errorMsgSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< arma::vec >::type m(mSEXP);
+    Rcpp::traits::input_parameter< arma::vec >::type Y(YSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type X(XSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type Z(ZSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type W(WSEXP);
+    Rcpp::traits::input_parameter< arma::vec >::type start(startSEXP);
+    Rcpp::traits::input_parameter< bool >::type trace(traceSEXP);
+    Rcpp::traits::input_parameter< bool >::type profile(profileSEXP);
+    Rcpp::traits::input_parameter< bool >::type errorMsg(errorMsgSEXP);
+    __result = Rcpp::wrap(acd_estimation(m, Y, X, Z, W, start, trace, profile, errorMsg));
+    return __result;
+END_RCPP
 }
 // hpc_estimation
-Rcpp::List hpc_estimation(arma::vec m, arma::vec Y, arma::mat X, arma::mat Z,
-                          arma::mat W, arma::vec start, bool trace,
-                          bool profile, bool errorMsg);
-RcppExport SEXP jmcm_hpc_estimation(SEXP mSEXP, SEXP YSEXP, SEXP XSEXP,
-                                    SEXP ZSEXP, SEXP WSEXP, SEXP startSEXP,
-                                    SEXP traceSEXP, SEXP profileSEXP,
-                                    SEXP errorMsgSEXP) {
-  BEGIN_RCPP
-  Rcpp::RObject __result;
-  Rcpp::RNGScope __rngScope;
-  Rcpp::traits::input_parameter<arma::vec>::type m(mSEXP);
-  Rcpp::traits::input_parameter<arma::vec>::type Y(YSEXP);
-  Rcpp::traits::input_parameter<arma::mat>::type X(XSEXP);
-  Rcpp::traits::input_parameter<arma::mat>::type Z(ZSEXP);
-  Rcpp::traits::input_parameter<arma::mat>::type W(WSEXP);
-  Rcpp::traits::input_parameter<arma::vec>::type start(startSEXP);
-  Rcpp::traits::input_parameter<bool>::type trace(traceSEXP);
-  Rcpp::traits::input_parameter<bool>::type profile(profileSEXP);
-  Rcpp::traits::input_parameter<bool>::type errorMsg(errorMsgSEXP);
-  __result = Rcpp::wrap(
-      hpc_estimation(m, Y, X, Z, W, start, trace, profile, errorMsg));
-  return __result;
-  END_RCPP
+Rcpp::List hpc_estimation(arma::vec m, arma::vec Y, arma::mat X, arma::mat Z, arma::mat W, arma::vec start, bool trace, bool profile, bool errorMsg);
+RcppExport SEXP jmcm_hpc_estimation(SEXP mSEXP, SEXP YSEXP, SEXP XSEXP, SEXP ZSEXP, SEXP WSEXP, SEXP startSEXP, SEXP traceSEXP, SEXP profileSEXP, SEXP errorMsgSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< arma::vec >::type m(mSEXP);
+    Rcpp::traits::input_parameter< arma::vec >::type Y(YSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type X(XSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type Z(ZSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type W(WSEXP);
+    Rcpp::traits::input_parameter< arma::vec >::type start(startSEXP);
+    Rcpp::traits::input_parameter< bool >::type trace(traceSEXP);
+    Rcpp::traits::input_parameter< bool >::type profile(profileSEXP);
+    Rcpp::traits::input_parameter< bool >::type errorMsg(errorMsgSEXP);
+    __result = Rcpp::wrap(hpc_estimation(m, Y, X, Z, W, start, trace, profile, errorMsg));
+    return __result;
+END_RCPP
 }
